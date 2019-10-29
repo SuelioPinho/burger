656e15b66b2b26964505e785e5e25bd0262d5ab5
Suelio Sousa <suelio@rav.eti.br>
2019-10-29 18:47:06 -0300

    - SideDrawer Component created - Added Firebase Realtime Database

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/Aux/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from '../../axios-orders';
 
 const INGREDIENTS_PRICES = {
     salad: 0.5,
@@ -78,7 +79,24 @@ class BurgerBuilder extends Component{
     };
 
     purchaseContinueHandler = () => {
-        alert('You continue!')
+        // alert('You continue!')
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Suelio Sousa',
+                address: {
+                    street: 'Rua Gilberto Studart, 55',
+                    zipCode: '60455-556',
+                    country: 'Brasil'
+                },
+                email: 'sueliotester@gmail.com'
+            },
+            deliveryMethod: 'fastest'
+        };
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     };
 
     render() {
